25df1bfc2b86119717084e3918fb48f6259c84f8
jrigassio <james.rigassio@gmail.com>
2017-01-22 10:45:10 -0800

    Add files via upload

---
+++ Tiny_Teacher_Vocabulary/src/question.js
@@ -0,0 +1,92 @@
+module.exports = [
+    {
+        'To abhor means to': [
+            'Loathe',
+            'Appreciate',
+            'Be fearful of',
+            'Love',
+        ],
+    },
+    {
+        'Placid means': [
+            'Calm',
+            'Somber',
+            'Violent',
+            'Stern',
+        ],
+    },
+    {
+    'What does abrasive mean?': [
+            'Rough',
+            'Silky',
+            'Spicy',
+            'Wet',
+        ],
+    },
+    {
+        'Define bilk': [
+            'cheat or defraud',
+            'Succeed',
+            'milk that is blue',
+            'Fail',
+        ],
+    },
+    {
+        'What does covert mean?': [
+            'Undercover',
+            'Closed',
+            'It is a type of spice',
+            'It is a political system',
+        ],
+    },
+    {
+        'What does engender mean?': [
+            'Cause',
+            'to change genders',
+            'Nothing',
+            'Sweaty',
+        ],
+    },
+    {
+        "Hangar": [
+            'Storage area, like a garage, for a plane',
+            'Executioner',
+            'warehouse',
+            'Positive',
+        ],
+    },
+    {
+        'Knotty': [
+            'Complex or difficult to solve',
+            'Bad',
+            'Sensual',
+            'Short',
+        ],
+    },
+    {
+        'Nuance': [
+            'Something subtle, a fine shade of meaning',
+            'Depressing',
+            'A type of fish',
+            'Viking',
+        ],
+    },
+    {
+        "Plagiarism": [
+            'Taking credit for someone else\'s writing or ideas',
+            'Melania Trump',
+            'Typing on a keyboard',
+            'The making of fine pottery',
+        ],
+    },
+    {
+        "Perfidy": [
+            'betrayal',
+            'a mystery',
+            'confusion',
+            'a problem',
+        ],
+    },
+    
+];
+
